7ea7d5407f5eb539c79140d8a8decbdb058d8f26
Amaterasu5 <mathmonkey4@gmail.com>
2013-08-01 15:40:44 -0400

    Filled in getWrappedData method, authFields now String[]

--- backend/TwitterWrapper.java
+++ backend/TwitterWrapper.java
@@ -1,25 +1,66 @@
 package backend;
 
-import java.util.ArrayList;
-
-public class TwitterWrapper extends AbstractGeoWrapper {
+public class TwitterWrapper extends AbstractDataWrapper {
 
 	/**
 	 * @param emageParams
 	 * @param wrapperParams
 	 * @param authFields
 	 */
-	public TwitterWrapper( WrapperParams wrapperParams,
-			AuthFields authFields, GeoParams geoParams) {
-		super( wrapperParams, authFields, geoParams);
-		// TODO add suport for CSV params
+    private
+    
+	public TwitterWrapper(
+			EmageParams emageParams, 
+			WrapperParams wrapperParams,
+			String[] authfields
+			) {
+		super(emageParams, wrapperParams, authFields);
+		
 	}
 
 	@Override
-	public
-	ArrayList<STTPoint> getWrappedData() {
-		// TODO Auto-generated method stub
-		return null;
+	PointStream getWrappedData() {
+	    FilterQuery query= new FilterQuery();
+        //query.track(keywords);
+        //query.locations(locations);
+        TwitterStream twitterStream= new TwitterStreamFactory().getInstance();
+        twitterStream.setOAuthConsumer(authFields[0],authFields[1]);
+        
+        twitterStream.setOAuthConsumer(authFields[2],authFields[3]);
+        
+        StatusListener listener = new StatusListener() {
+            @Override
+            public void onStatus(Status status) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+                System.out.println("Got stall warning:" + warning);
+            }
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+        twitterStream.addListener(listener);
+        twitterStream.filter(query);
 	}
 
 }
