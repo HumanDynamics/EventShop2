55b7def2288fa63bc5d0625f761f0cd85b31dda2
Amaterasu5 <mathmonkey4@gmail.com>
2013-08-08 16:24:03 -0400

    simple idea of what the AuthFields class could look like/getters

--- backend/AuthFields.java
+++ backend/AuthFields.java
@@ -1,5 +1,29 @@
 package backend;
 
 public class AuthFields {
-
+    
+    private String accessToken;
+    private String accessTokenSecret;
+    private String consumerKey;
+    private String consumerKeySecret;
+    
+    public AuthFields(String accessToken,String accessTokenSecret,String consumerKey,String consumerKeySecret){
+        this.accessToken = accessToken;
+        this.accessTokenSecret = accessTokenSecret;
+        this.consumerKey = consumerKey;
+        this.consumerKeySecret = consumerKeySecret;
+    }
+    
+    public String getAccessToken(){
+        return this.accessToken;
+    }
+    public String getAccessTokenSecret(){
+        return this.accessTokenSecret;
+    }
+    public String getConsumerKey(){
+        return this.consumerKey;
+    }
+    public String getConsumerKeySecret(){
+        return this.consumerKeySecret;
+    }
 }
